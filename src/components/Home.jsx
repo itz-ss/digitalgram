18f73382cb92eb7eb8ceb58a4fbb5b3c1cbcfc0a
Saransh Singh <saranshsingh2205@gmail.com>
2026-01-12 12:36:01 +0530

    second

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,14 +3,12 @@ import Services from "./Services";
 import HowWeWork from "./HowWeWork";
 import Contact from "./Contact";
 import HeroClip from "./HeroClip";
-import GlobalGlass from "./GlobalGlass";
 import AppBackground from "./AppBackground";
 
 const Home = () => {
   return (
     <>
       <AppBackground />
-      {/* <GlobalGlass /> */}
       <Welcome />
       <HeroClip />
       <Services />
